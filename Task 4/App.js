c4b29a7efea5ed3a2eb5990bff630d2b38e4b989
ShinyAnjana <anjanashiny1@gmail.com>
2024-06-15 10:01:53 +0530

    Add files via upload

---
+++ Task 4/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/Todo.js'; 
+function App() {
+  return (
+    <div className="App container my-3 mx-8 "   style={{ backgroundColor:'#DDA0DD'}}>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
+  
